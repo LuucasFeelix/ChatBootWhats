a86e838662e24fbfc90be76177acc0093d5dcc4c
Lucas Felix Da Silva <luucasfeelix1925@hotmail.com>
2025-02-24 20:59:23 -0300

    Foi criado class ChatHub e configurado o SignalR no  Program.cs

--- ChatBootWhatsapp/Hubs/ChatHub.cs
+++ ChatBootWhatsapp/Hubs/ChatHub.cs
@@ -1,6 +1,12 @@
-﻿namespace ChatBootWhatsapp.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatBootWhatsapp.Hubs
 {
-    public class ChatHub
+    public class ChatHub : Hub
     {
+        public async Task NotifyNewMessage(string userId, string message)
+        {
+            await Clients.User(userId).SendAsync("ReceberMenssagem", message);
+        }
     }
 }
